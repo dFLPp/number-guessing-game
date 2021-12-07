ac8c7d327e798272c4615d4138f150393956e353
dFLPp <felipdsousc@gmail.com>
2021-12-07 14:40:07 -0300

    adding theme feature

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 
 const startGame = document.querySelector(".game-start");
 const gameBox = document.querySelector(".game-container");
+const selectionBox = document.querySelector("#theme");
+const buttonOpenSelection = document.querySelector(".chang-theme");
 
 startGame.addEventListener("click", function() {
     gameBox.style["display"] = "flex";
     startGame.style["display"] = "none";
+    if(selectionBox.style["display"]==="block"){
+      selectionBox.style["display"]="none"
+    }
+    //buttonOpenSelection.style["display"] = "none";
 })
 
 let randomNumber = Math.floor(Math.random() * 100) + 1;
@@ -85,6 +91,28 @@ function resetGame() {
     turno++
 }
 
-
-
 enviarInput.addEventListener("click", verificarResposta)
+
+const selectOption = document.querySelector('select');
+const allC = document.querySelector('.all-content');
+const html = document.querySelector("html")
+const apresentation = document.querySelector(".apresentation")
+
+function update(bgColor, textColor, especial, buttonB, buttonC) {
+  allC.style.backgroundColor = bgColor;
+  allC.style.color = textColor;
+  html.style.backgroundColor = bgColor;
+  apresentation.style.color = textColor;
+  apresentation.style.backgroundColor = especial;
+  startGame.style.backgroundColor = buttonB;
+  startGame.style.color = buttonC;
+}
+
+selectOption.onchange = function() {
+  (selectOption.value === "black") ? update("rgb(26, 0, 51)","white", "gray", "rgb(102, 0, 0)", "white") : update("rgb(230, 255, 230)","black", "white", "rgb(102, 255, 204)", "black");
+}
+
+buttonOpenSelection.addEventListener("click", function(){
+  selectionBox.style["display"]="block";
+  buttonOpenSelection.style["display"]="none";
+})
