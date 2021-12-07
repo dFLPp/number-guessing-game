19e4f0a5afadf38a946a8bf9c1fe5734ac6718d0
dFLPp <felipdsousc@gmail.com>
2021-12-07 16:03:47 -0300

    deixando apresentável

--- main.js
+++ main.js
@@ -3,26 +3,30 @@ const startGame = document.querySelector(".game-start");
 const gameBox = document.querySelector(".game-container");
 const selectionBox = document.querySelector("#theme");
 const buttonOpenSelection = document.querySelector(".chang-theme");
-
-startGame.addEventListener("click", function() {
-    gameBox.style["display"] = "flex";
-    startGame.style["display"] = "none";
-
-})
-
+const prettyPara = document.querySelector(".ordem");
 let randomNumber = Math.floor(Math.random() * 100) + 1;
-
 const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
 const hint = document.querySelector('.hint');
-
 const enviarInput = document.querySelector('.enviarInput');
 const campoInput = document.querySelector('.campoInput');
-
 let turno = 1;
 let resetButton;
 
+guesses.style["display"] ="none";
+lastResult.style["display"] ="none";
+hint.style["display"] ="none";
+
+startGame.addEventListener("click", function() {
+  gameBox.style["display"] = "flex";
+  startGame.style["display"] = "none";
+
+})
+
 function verificarResposta() {
+    lastResult.style["display"] ="block";
+    hint.style["display"] ="block";
+    guesses.style["display"] = "block";
     let userGuess = Number(campoInput.value);
     if (turno === 1) {
       guesses.textContent = 'Sugestões anteriores: ';
@@ -39,8 +43,8 @@ function verificarResposta() {
     } else if (turno === 10) {
       lastResult.textContent = 'Você perdeu :(';
       lastResult.style.backgroundColor = 'red';
-      hint.textContent = '';
-      document.getElementById("hint").style["display"] ="none";
+      document.getElementById("hint").style["display"] ="block";
+      hint.textContent = 'O número era: ' + randomNumber;
       setGameOver();
     } else {
       lastResult.textContent = 'Errou!';
@@ -66,6 +70,8 @@ function setGameOver() {
     document.getElementById("game-start").style["color"] ="black";
     document.getElementById("game-start").appendChild(resetButton);
     resetButton.addEventListener('click', resetGame);
+    prettyPara.textContent = ""
+    gameBox.style["filter"] = "brightness(80%)";
 }
 
 function resetGame() {
@@ -79,14 +85,20 @@ function resetGame() {
     resetButton.parentNode.removeChild(resetButton);
     document.getElementById("game-start").style["display"] ="none";
     lastResult.style.backgroundColor = 'black';
-    document.getElementById("hint").style["display"] ="flex";
+
+    hint.style["display"] ="none";
+    guesses.style["display"] ="none";
+    lastResult.style["display"] ="none";
+
     campoInput.disabled = false;
     enviarInput.disabled = false;
     campoInput.value = '';
     campoInput.focus();
-  
+    prettyPara.textContent = "Dê um palpite: "
+    gameBox.style["filter"] = "brightness(100%)";
     randomNumber = Math.floor(Math.random() * 100) + 1;
     turno++
+    guesses.textContent = 'Sugestões anteriores: ';
 }
 
 enviarInput.addEventListener("click", verificarResposta)
@@ -96,7 +108,7 @@ const allC = document.querySelector('.all-content');
 const html = document.querySelector("html")
 const apresentation = document.querySelector(".apresentation")
 
-function update(bgColor, textColor, especial, buttonB, buttonC) {
+function update(boxColor, bgColor, textColor, especial, buttonB, buttonC) {
   allC.style.backgroundColor = bgColor;
   allC.style.color = textColor;
   html.style.backgroundColor = bgColor;
@@ -104,10 +116,12 @@ function update(bgColor, textColor, especial, buttonB, buttonC) {
   apresentation.style.backgroundColor = especial;
   startGame.style.backgroundColor = buttonB;
   startGame.style.color = buttonC;
+  gameBox.style.backgroundColor = boxColor;
+  prettyPara.style.color = textColor;
 }
 
 selectOption.onchange = function() {
-  (selectOption.value === "black") ? update("rgb(26, 0, 51)","white", "gray", "rgb(102, 0, 0)", "white") : update("rgb(230, 255, 230)","black", "white", "rgb(102, 255, 204)", "black");
+  (selectOption.value === "black") ? update("rgb(0, 26, 102)","rgb(26, 0, 51)","white", "gray", "rgb(102, 0, 0)", "white") : update("rgb(255, 219, 77)","rgb(230, 255, 230)","black", "white", "rgb(102, 255, 204)", "black");
 }
 
 buttonOpenSelection.addEventListener("click", function(){
